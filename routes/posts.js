44bbb9b0521f499ef88bde1b77661154b1631e4c
malkamat <rimka.matkalo@gmail.com>
2020-10-26 18:26:43 +0100

    ajout routes update post et delete post dans posts.js et fin de projet

--- routes/posts.js
+++ routes/posts.js
@@ -1,3 +1,4 @@
+const e = require("express")
 const express = require("express")
 
 const router = express.Router()
@@ -8,23 +9,75 @@ router.get("/", async (req, res) => {
         const posts = await Post.find()
         res.json(posts)
     } catch (error) {
-        res.json({message: error})
+        res.json({
+            message: error
+        })
     }
 })
 
-router.post("/" , async (req,res) => {
+//submit a post
+router.post("/", async (req, res) => {
     const post = new Post({
         title: req.body.title,
         description: req.body.description
     })
     try {
 
-    const savedPost = await post.save()
-    res.json(savedPost)
+        const savedPost = await post.save()
+        res.json(savedPost)
     } catch (error) {
-        res.json({message: error})
+        res.json({
+            message: error
+        })
     }
-   
+
+
+    //specific post
+    router.get("/:postId", async (req, res) => {
+        try {
+            const post = await Post.findById(req.params.postId)
+            res.json(post)
+        } catch (error) {
+            res.json({
+                message: error
+            })
+        }
+
+    })
+
+})
+
+
+//delete a specific post
+router.delete("/:postId", async (req, res) => {
+    try {
+        const removePost = await Post.remove({
+            _id: req.params.postId
+        })
+        res.json(removePost)
+    } catch (error) {
+        res.json({
+            message: error
+        })
+    }
+})
+
+//Update a post
+router.patch("/postId", async (req, res) => {
+    try {
+        const updatedPost = await Post.updateOne({
+            _id: req.params.postId
+        }, {
+            $set: {
+                title: req.body.title
+            }
+        })
+        res.json(updatedPost)
+    } catch (error) {
+        res.json({
+            message: error
+        })
+    }
 })
 
 module.exports = router
